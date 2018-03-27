e3901a8cc63299315e0825bab8ce900f1d51a2ba
Māris Krivtežs <marisks@gmail.com>
2018-03-27 16:24:41 +0300

    Cleanup.

--- src/FeaturesViewEngine/DefaultControllerFeaturesViewEngine.cs
+++ src/FeaturesViewEngine/DefaultControllerFeaturesViewEngine.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq;
-
-namespace FeaturesViewEngine
+﻿namespace FeaturesViewEngine
 {
     public sealed class DefaultControllerFeaturesViewEngine : ControllerFeaturesViewEngine
     {
