b77a428a35a2013621e269a30a9d8f875652f4cb
Māris Krivtežs <marisks@gmail.com>
2018-04-21 12:50:28 +0300

    Updated year info.

--- src/FeaturesViewEngine.Build/AssemblyInfo.fs
+++ src/FeaturesViewEngine.Build/AssemblyInfo.fs
@@ -12,7 +12,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("FeaturesViewEngine.Build")>]
-[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyCopyright("Copyright ©  2018")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
 
